d6e2084a326893d341ec6835377074f0c9d16b65
nolan <emirot.nolan@gmail.com>
2014-11-11 20:53:42 +0100

    classifying things

--- theGridSearch/ifin.py
+++ theGridSearch/ifin.py
@@ -8,11 +8,11 @@ if b in a:
 else:
     print("false")
 
-for n in range(2, 19):
-    for x in range(2, n):
-        print("n%x",n,x,n%x)
-        if n % x == 0:
-             print(n, 'equals', x, '*', n/x)
-             break
-    else:
-        print(n, 'is a prime number')
+
+arra = [1,2,4,5,6]
+arr3 = [2,4,5]
+
+if arr3 in arra:
+    print("†ue")
+else:
+    print("falses")
