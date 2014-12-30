175e875cb21a1ddeede05cc571bc473afcb34d2a
nolan <emirot.nolan@gmail.com>
2014-12-30 23:27:07 +0100

    adding file

---
+++ arrayAndSorting/quickSortInPlace/quickSortInPlace.py
@@ -0,0 +1,39 @@
+__author__ = 'nolanemirot'
+
+
+
+def swapIndexes(arr, index1, index2):
+    print("index1",index1)
+    print("index2",index2)
+    tmp = arr[index1]
+    arr[index1] = arr[index2]
+    arr[index2] = tmp
+    return arr
+
+
+
+def quickSortInPlace(arr):
+    greaterPassed = False
+    pivot = arr[len(arr)-1]
+    print("pivot:",pivot)
+
+    for i, a in enumerate(arr):
+        if a > pivot:
+            greaterPassed = True
+            print(greaterPassed)
+        if a < pivot and greaterPassed == True:
+            j = i
+            print("LALAL")
+            print("J", j)
+            while pivot < arr[j-1] :
+                j -= 1
+            print("JJ",j)
+            arr = swapIndexes(arr,i,j)
+        print(arr)
+
+
+if __name__ == '__main__':
+    nbNum =  int(input())
+    line = input()
+    arr = list(map(int,line.split()))
+    quickSortInPlace(arr)
