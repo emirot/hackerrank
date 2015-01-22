dc9ffb33fafa86907fb62e60ea94eee5f5dad827
nolan <emirot.nolan@gmail.com>
2015-01-22 11:34:04 +0100

    add findhackerrank

--- miscellaneous/regex/findHackerRank.py
+++ miscellaneous/regex/findHackerRank.py
@@ -1,1 +1,21 @@
-v
+__author__ = 'nolanemirot'
+import  re
+
+def findHackerRank(give):
+    if re.search("^hackerrank$",give,re.IGNORECASE):
+        print("0")
+    elif re.search("hackerrank$",give,re.IGNORECASE):
+        print("2")
+    elif re.search("^hackerrank",give,re.IGNORECASE):
+        print("1")
+    else:
+        print("-1")
+
+
+if __name__ == '__main__':
+    nbTest = int(input())
+    i = 0
+    while i < nbTest:
+        give = input()
+        findHackerRank(give)
+        i += 1
