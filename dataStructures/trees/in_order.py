e77527ca5a4e1d0f9106d22847c597da0bd1ab4d
Nolan Emirot <nolan@turo.com>
2017-02-08 07:54:23 -0800

    Create in_order.py

---
+++ dataStructures/trees/in_order.py
@@ -0,0 +1,15 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+
+def inOrder(root):
+    if root:
+        inOrder(root.left)
+        print(root.data, end=" ")
+        inOrder(root.right)
+        
+        
+
